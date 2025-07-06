1bd2655d34be1261f6d648b7b9aa1d2b89329e7e
noqx <noel.merz09@outlook.com>
2025-07-06 18:54:46 +0200

    init

---
+++ Minecraft Server Setup/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Runtime.InteropServices;
+
+internal class Program
+{
+
+    private static void Main(string[] args)
+    {
+        loader();
+    }
+
+    static void eula()
+    {
+        Console.WriteLine("By running this server, you agree to the EULA.");
+        Console.WriteLine("You can find the EULA at https://www.minecraft.net/en-us/eula");
+        Console.WriteLine("Press any key to continue...");
+        Console.ReadKey();
+    }
+
+    static void loader()
+    {
+        int listNumber = 1;
+
+        var loaderVar = new loader[]
+        {
+            new("Vanilla"),
+            new("Spigot"),
+            new("Paper"),
+            new("Fabric"),
+        };
+
+        foreach (var loader in loaderVar )
+        {
+            Console.WriteLine($"({listNumber}) {loader.name}");
+            listNumber++;
+        }
+
+        Console.Write("\nPlease select a loader: ");
+        ConsoleKeyInfo keyInput = Console.ReadKey();
+
+        int selectedNumber = keyInput.KeyChar - '0';
+        if (keyInput.KeyChar != listNumber || keyInput.KeyChar > loaderVar.Length)
+        {
+            Console.WriteLine($"\n\ndebug: {loaderVar[selectedNumber - 1].name}");
+        }
+
+    }
+
+}
+
+public record Vanilla(string Name, string DownloadUrl);
+public record Spigot(string Name, string DownloadUrl);
+public record Paper(string Name, string DownloadUrl);
+public record Fabric(string Name, string DownloadUrl);
+
+public record loader(string name);
